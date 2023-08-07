c0a638b5c767808414849851b495def8cd7c4848
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-08-07 16:10:26 +0300

    chore: refactor TranscriptVersion

--- src/main/java/school/hei/haapi/service/StudentTranscriptVersionService.java
+++ src/main/java/school/hei/haapi/service/StudentTranscriptVersionService.java
@@ -1,5 +1,6 @@
 package school.hei.haapi.service;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -7,13 +8,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.BoundedPageSize;
 import school.hei.haapi.model.PageFromOne;
-import school.hei.haapi.model.StudentTranscriptClaim;
 import school.hei.haapi.model.StudentTranscriptVersion;
 import school.hei.haapi.model.exception.NotFoundException;
 import school.hei.haapi.repository.StudentTranscriptVersionRepository;
-
-import java.util.List;
-import java.util.Objects;
 
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
@@ -24,16 +21,20 @@ public class StudentTranscriptVersionService {
   private final StudentTranscriptVersionRepository studentTranscriptVersionRepository;
 
   public StudentTranscriptVersion getByIdAndStudentIdAndTranscriptId(
-          String versionId, String transcriptId, String studentId) {
-    return studentTranscriptVersionRepository.getByIdAndStudentIdAndTranscriptId(versionId, transcriptId, studentId);
+      String versionId, String transcriptId, String studentId) {
+    return
+        studentTranscriptVersionRepository.getByIdAndStudentIdAndTranscriptId(versionId,
+                transcriptId, studentId)
+            .orElseThrow(() -> new NotFoundException("Version.Id=" + versionId + " not found."));
   }
 
   public List<StudentTranscriptVersion> getAllByStudentIdAndTranscriptId(
-          String studentId, String transcriptId, PageFromOne page, BoundedPageSize pageSize){
+      String studentId, String transcriptId, PageFromOne page, BoundedPageSize pageSize) {
     Pageable pageable = PageRequest.of(
-            page.getValue() - 1,
-            pageSize.getValue(),
-            Sort.by(DESC, StudentTranscriptVersion.CREATION_DATETIME));
-    return studentTranscriptVersionRepository.getAllByStudentIdAndTranscriptId(studentId, transcriptId, pageable);
+        page.getValue() - 1,
+        pageSize.getValue(),
+        Sort.by(DESC, StudentTranscriptVersion.CREATION_DATETIME));
+    return studentTranscriptVersionRepository.getAllByStudentIdAndTranscriptId(studentId,
+        transcriptId, pageable);
   }
 }

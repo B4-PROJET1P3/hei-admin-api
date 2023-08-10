66cf8612bea6978e54a1265184a6ad6c4f681063
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-08-10 14:13:15 +0300

    chore: add creation timestamp annotation

--- src/main/java/school/hei/haapi/model/StudentTranscriptVersion.java
+++ src/main/java/school/hei/haapi/model/StudentTranscriptVersion.java
@@ -1,8 +1,18 @@
 package school.hei.haapi.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -15,20 +25,21 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StudentTranscriptVersion implements Serializable {
-  @Id
-  private String id;
+    @Id
+    private String id;
 
-  @ManyToOne
-  @JoinColumn(name = "transcript_id")
-  private Transcript transcript;
+    @ManyToOne
+    @JoinColumn(name = "transcript_id")
+    private Transcript transcript;
 
-  private Integer ref;
+    private Integer ref;
 
-  @ManyToOne
-  @JoinColumn(name = "responsible_id")
-  private User responsible;
+    @ManyToOne
+    @JoinColumn(name = "responsible_id")
+    private User responsible;
 
-  private Instant creationDatetime;
+    @CreationTimestamp
+    private Instant creationDatetime;
 
-  public static final String CREATION_DATETIME = "creationDatetime";
+    public static final String CREATION_DATETIME = "creationDatetime";
 }
